a01cc52d188c183445134e6b20e33729b08d6640
vNmven0m <danielbruch631@mail.com>
2024-12-02 15:03:30 +0100

    Day 1

---
+++ Day 1/day1.py
@@ -0,0 +1,19 @@
+a = []
+b = []
+
+with open("Day 1\input\input") as f:
+    for line in f:
+        x, y = line.strip().split("   ")
+        a.append(int(x))
+        b.append(int(y))
+
+a.sort()
+b.sort()
+
+i = 0
+r = 0
+while i <= (len(a)-1):
+    r += abs(a[i]-b[i])
+    i += 1
+
+print(r)
