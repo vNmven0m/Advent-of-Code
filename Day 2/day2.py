325e312ee7b54c5cf0d6870e97f7dd8fb26c3474
vNmven0m <danielbruch631@gmail.com>
2024-12-07 00:59:40 +0100

    Day 2 Part 2

--- Day 2/day2.py
+++ Day 2/day2.py
@@ -1,14 +1,24 @@
 count = count2 = 0
 
-with open("Day 2\input\input") as f:
+
+def check_valid(report):
+    if all((1 <= abs(report[i]-report[i+1]) <= 3) for i in range(len(report) -1 )) and (all(report[i] <= report[i + 1] for i in range(len(report) - 1)) or all(report[i] >= report[i + 1] for i in range(len(report) - 1))):
+        return True
+    return False
+
+
+with open("Day 2/input/input") as f:
     for line in f:
         report = list(map(int,(line.strip().split(" "))))
-        if all((1 <= abs(report[i]-report[i+1]) <= 3) for i in range(len(report) -1 )) and (all(report[i] <= report[i + 1] for i in range(len(report) - 1)) or all(report[i] >= report[i + 1] for i in range(len(report) - 1))):
+        if check_valid(report):
             count += 1
             continue
-        if all(report[i] <= report[i + 1] for i in range(len(report) - 1)) or all(report[i] >= report[i + 1] for i in range(len(report) - 1)) and 0 <= len(report)-len(list(dict.fromkeys(report))) <= 1:
-            dis = []
-            for i in range(len(report)-1):
-                dis.append(report[i]-report[i+1])
-            print(dis)
-print(count)
+        for i in range(len(report)):
+            temp = report[:i] + report[i + 1:]
+            if check_valid(temp):
+                count2 += 1
+                break
+                
+
+print("Part 1:", count)
+print("Part 2:", count + count2)
